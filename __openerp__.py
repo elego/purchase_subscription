9be81ae6ea657ae4d04a5e4a0dd3d309c022c93a
Nicolas P <nicolas.potier@sudokeys.com>
2016-11-04 11:37:19 +0100

    Initial commit

---
+++ __openerp__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+{
+    'name' : 'Purchase Subcription',
+    'installable' : True,
+    'application' : True,
+    'depends' : ['base'],
+    'data' : [
+             ],
+    'author' : 'sudokeys',
+}
