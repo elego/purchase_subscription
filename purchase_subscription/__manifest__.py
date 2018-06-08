b347b8931ea8212602233d4e758fdec952027f29
Rémi FRANÇOIS <remi@sudokeys.com>
2018-06-08 09:13:41 +0000

    [MIG] set uninstalable

--- purchase_subscription/__manifest__.py
+++ purchase_subscription/__manifest__.py
@@ -4,7 +4,7 @@
     'name': 'Purchase Subscription',
     'summary': "An easy way to manage your provider's subscriptions.",
     'category': 'Purchases',
-    'installable': True,
+    'installable': False,
     'application': True,
     'license' : 'AGPL-3',
     'depends': ['base', 'account', 'analytic', 'purchase'],
